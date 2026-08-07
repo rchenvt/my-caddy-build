ea1d89c76f4842406d299db5942d73cc240b670e
rchenvt <134825510+rchenvt@users.noreply.github.com>
2026-08-07 17:00:13 -0400

    Delete healthcheck.sh

--- healthcheck.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Authelia API Check
-wget -q --spider http://localhost:9091/api/health || exit 1
-# LLDAP Binary Check
-lldap healthcheck --config-file /data/lldap/lldap_config.toml > /dev/null || exit 1
-# Inbucket Port Check
-wget -q --spider http://localhost:9000 || exit 1
-# Caddy Port Check
-wget -q --spider http://localhost:2019/metrics || exit 1
